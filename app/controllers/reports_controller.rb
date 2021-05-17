78702d7ee71a0c0fe55a1c7d3075b56b96727767
KENTA MITANI <mitani.kenta88@gmail.com>
2021-05-17 11:44:03 +0900

    itemテーブル実装

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,12 @@
+class ReportsController < ApplicationController
+  def index
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    Item.create
+  end
+end
---
+++ db/migrate/20210516065748_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.string :item_name
+      t.string :item_status
+
+      t.timestamps
+    end
+  end
+end
