eb5c09764d8c025aa11a63bfc98afad44cd6a781
KENTA MITANI <mitani.kenta88@gmail.com>
2021-05-17 11:44:03 +0900

    itemテーブル実装

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,18 @@
+class ItemsController < ApplicationController
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_status)
+  end
+end
